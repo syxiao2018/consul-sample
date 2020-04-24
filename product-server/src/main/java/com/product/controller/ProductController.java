3793455b4eb539d8020fef2a941385632588c74c
syxiao <syxiao@thoughtworks.com>
2020-04-24 10:54:47 +0800

    add config server

--- product-server/src/main/java/com/product/controller/ProductController.java
+++ product-server/src/main/java/com/product/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.product.controller;
 import com.product.config.ConfigProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/product")
 @RequiredArgsConstructor
+@RefreshScope
 public class ProductController {
     @Value("${spring.application.name}")
     private String appName;
